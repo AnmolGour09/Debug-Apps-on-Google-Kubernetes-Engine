d690885f238c7d9fe05830b9bb3c4713a0f74a7b
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-04-02 18:04:23 +0530

    Create Anmol.sh

---
+++ Anmol.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+TEAL=$'\033[38;5;50m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+BLINK_TEXT=$'\033[5m'
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+REVERSE_TEXT=$'\033[7m'
+
+clear
+
+echo "${BG_BLUE}${WHITE}${BOLD}=====================================================${RESET}"
+echo "${BG_BLUE}${WHITE}${BOLD}         Google cloud with Anmol Gour                                   ${RESET}"
+echo "${BG_BLUE}${WHITE}${BOLD}=====================================================${RESET}"
+echo ""
+
+gcloud config set compute/zone $ZONE
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+
+gcloud container clusters get-credentials central --zone $ZONE
+
+git clone https://github.com/xiangshen-dk/microservices-demo.git
+cd microservices-demo
+
+kubectl apply -f release/kubernetes-manifests.yaml
+
+sleep 30
+
+gcloud logging metrics create Error_Rate_SLI \
+  --description="Error rate for recommendationservice" \
+  --log-filter="resource.type=\"k8s_container\" severity=ERROR labels.\"k8s-pod/app\": \"recommendationservice\""
+
+sleep 30
+
+cat > awesome.json <<EOF_END
+{
+  "displayName": "Error Rate SLI",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "Kubernetes Container - logging/user/Error_Rate_SLI",
+      "conditionThreshold": {
+        "filter": "resource.type = \"k8s_container\" AND metric.type = \"logging.googleapis.com/user/Error_Rate_SLI\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 0.5
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "604800s"
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_END
+
+gcloud alpha monitoring policies create --policy-from-file="awesome.json"
+
+# Final message
+echo
+echo "${BG_RED}${WHITE}${BOLD}=====================================================${RESET}"
+echo "${BG_RED}${WHITE}${BOLD}  🔔 Subscribe to Anmol Gour on YouTube 🔔          ${RESET}"
+echo "${BG_RED}${WHITE}${BOLD}=====================================================${RESET}"
+echo
+
+echo
